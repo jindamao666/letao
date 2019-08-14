532c32ac73d018df5dae59efe9f5c31deaeecb48
jindamao666 <824915954@qq.com>
2019-08-14 22:36:56 +0800

    登录页面静态页完成

---
+++ public/back/js/login.js
@@ -0,0 +1,43 @@
+$(function () {
+    // 1. 进行表单校验
+    //    校验要求: (1) 用户名不能为空
+    //              (2) 密码不能为空, 且必须是 6-12 位
+
+    //配置的字段和input框中指定的name关联，所以必须要给input加上name
+    $("#form").bootstrapValidator({
+        //配置字段
+        fields:{
+            username:{
+            // 配置校验规则
+                validators:{
+                    //非空
+                    notEmpty:{
+                        //提示信息
+                        message:"用户名不能为空"
+                    },
+                    //长度校验
+                    stringLength: {
+                        min: 2,
+                        max: 6,
+                        message: '用户名长度必须在2到6之间'
+                    }
+                }
+            },
+            password:{
+                validators:{
+                    //非空
+                    notEmpty:{
+                        //提示信息
+                        message:"密码不能为空"
+                    },
+                    //长度校验
+                    stringLength: {
+                        min: 6,
+                        max: 12,
+                        message: '用户名长度必须在6到12之间'
+                    }
+                }
+            }
+        }
+    });
+});
