54dc994992cf75f3f0babb6a46e1520fa73e4789
jindamao666 <824915954@qq.com>
2019-08-15 11:59:29 +0800

    ajax登录校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -27,6 +27,10 @@ $(function () {
                         min: 2,
                         max: 6,
                         message: '用户名长度必须在2到6之间'
+                    },
+                    //专门用于配置回调提示的规则
+                    callback:{
+                        message:'用户名不存在'
                     }
                 }
             },
@@ -42,9 +46,62 @@ $(function () {
                         min: 6,
                         max: 12,
                         message: '用户名长度必须在6到12之间'
+                    },
+                    //专门用于配置回调提示的规则
+                    callback:{
+                        message:'密码错误'
                     }
                 }
             }
         }
+    });
+    /*2.登录功能
+    * 表单校验插件会在提交表单时进行校验
+    * （1） 校验成功 默认就提交表单，会发生页面跳转 ，注册表单校验成功事件，阻止默认的提交，通过ajax进行发送请求
+    * （2） 校验失败 ，不会提交表单，按照插件提示用户即可
+    * */
+    //注册表单校验成功事件
+    $("#form").on('success.form.bv',function (e) {
+        //阻止默认的表单提交
+        e.preventDefault();
+        // console.log("校验成功后的表单提交被阻止了");
+        //通过ajax进行提交
+        $.ajax({
+            type:"post",
+            url:"/employee/employeeLogin",
+            data:$('#form').serialize(),
+            dataType:"json",
+            success:function ( info ) {
+                console.log( info );
+                if( info.success ){
+                    //登录成功，跳转到首页
+                    location.href = "index.html"
+                }
+                if( info.error === 1000 ){
+                    // alert("当前用户名不存在");
+                    //updateStatus  更新校验状态
+                    //1.字段名称
+                    //2.校验状态
+                    // + NOT_VALIDATED：未校验的
+                    // + VALIDATING：校验中的
+                    // + INVALID ：校验失败的
+                    // + VALID：校验成功的。
+                    //3.校验规则，用于指定提示文本
+                    $('#form').data("bootstrapValidator").updateStatus("username","INVALID","callback");
+                }
+                if( info.error === 1001 ){
+                    // alert("密码错误");
+                    $('#form').data("bootstrapValidator").updateStatus("password","INVALID","callback");
+                }
+            }
+        })
+    });
+
+    //重置功能
+    $('[type="reset"]').click(function () {
+        //调用插件的方法，进行重置校验状态
+        //传true，重置内容以及校验状态
+        //传false，重置校验状态
+        $('#form').data("bootstrapValidator").resetForm(true);
     });
 });
