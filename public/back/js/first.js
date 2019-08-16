2fd271dc1f39eafda007d8d7ae1a42f43b1bdead
jindamao666 <824915954@qq.com>
2019-08-16 15:32:55 +0800

    一级分类完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -1,7 +1,7 @@
 $(function () {
     var currentPage = 1;//当前页
     var pageSize = 5;//每页条数
-    //1.一进入页面，发送ajax请求获取数据，通过模版引擎渲染
+//1.一进入页面，发送ajax请求获取数据，通过模版引擎渲染
     render();
     function render() {
         $.ajax({
@@ -36,5 +36,61 @@ $(function () {
                 })
             }
         })
-    }
-});
+    };
+//2.点击添加分类按钮，显示模态框
+    $('#addBtn').click(function () {
+        $('#addModal').modal("show");
+    });
+//3.使用表单校验插件，实现表单校验
+    $('#form').bootstrapValidator({
+        //配置图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',//校验成功
+            invalid: 'glyphicon glyphicon-remove',//校验失败
+            validating: 'glyphicon glyphicon-refresh'//校验中
+        },
+        //配置字段
+        fields:{
+            categoryName:{
+                // 配置校验规则
+                validators:{
+                    //非空
+                    notEmpty:{
+                        //提示信息
+                        message:"一级分类不能为空"
+                    }
+                }
+            }
+        }
+    });
+/* 4.表单校验插件会在提交表单时进行校验
+    * （1） 校验成功 默认就提交表单，会发生页面跳转 ，注册表单校验成功事件，阻止默认的提交，通过ajax进行发送请求
+    * （2） 校验失败 ，不会提交表单，按照插件提示用户即可
+    * */
+    //注册表单校验成功事件
+    $("#form").on('success.form.bv',function (e) {
+        //阻止默认的表单提交
+        e.preventDefault();
+        // console.log("校验成功后的表单提交被阻止了");
+        //通过ajax进行提交
+        $.ajax({
+            type:"post",
+            url:"/category/addTopCategory",
+            data:$('#form').serialize(),
+            dataType:"json",
+            success:function ( info ) {
+                console.log( info );
+                if(info.success){
+                    //添加成功
+                    // 1.关闭模态框
+                    $('#addModal').modal("hide");
+                    //2.页面重新渲染第一页，让用户看到第一页的数据
+                    currentPage = 1;
+                    render();
+                    //重置模态框
+                    $('#form').data("bootstrapValidator").resetForm(true);
+                }
+            }
+        })
+    });
+})
