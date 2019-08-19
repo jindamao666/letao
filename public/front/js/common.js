118b025cb65a62c386a6795b3d017c9211e52f15
jindamao666 <824915954@qq.com>
2019-08-19 19:46:16 +0800

    移动端-首页完成

---
+++ public/front/js/common.js
@@ -0,0 +1,11 @@
+//区域滚动
+mui('.mui-scroll-wrapper').scroll({
+    indicators: false, //是否显示滚动条
+    deceleration: 0.0005 //阻尼系数,系数越小滑动越灵敏
+});
+//配置轮播图自动轮播
+//获得slider插件对象
+var gallery = mui('.mui-slider');
+gallery.slider({
+    interval:5000//自动轮播周期，若为0则不自动播放，默认为0；
+});
